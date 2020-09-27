296924d5e5bc97f468116fa0e6f904ad509f6712
cemdalay <71180527+cemdalay@users.noreply.github.com>
2020-09-27 13:04:52 +0300

    Update Odev1.py

--- YMS4585/introduction/Odev/Odev1.py
+++ YMS4585/introduction/Odev/Odev1.py
@@ -17,3 +17,70 @@
 
 # Yeni bir işlem yapmak istiyor musunuz (Y/N) :
 ###############################################
+print("""
+###############################################
+#                                             # 
+#                                             # 
+#             Telefon Rehberi                 #
+#                                             #
+#                                             #
+#                                             #
+###############################################
+""")
+
+try:
+    
+    telefon_rehberi = ["kayıt ekleme","kayıt düzenleme","kayıt silme","kayıt listesi","arama"]
+    
+    
+    kayit_ekleme = int(input("Kayıt eklemek için 1'e basınız : "))
+    
+    if kayit_ekleme == 1 :
+        isim = input("Lütfen kaydetmek istediğiniz ismi giriniz :")
+        soyadı = input("Lütfen kaydetmek istediğiniz soyadını giriniz : ")
+        tel_no = input("Lütfen kaydetmek istediğiniz telefon numaranısını giriniz : ")
+
+        print(f"""
+        Adı                 : {isim}
+        Soyadı              : {soyadı}
+        Mail Adresi         : {isim}{soyadı}@cdcable.com
+        Telefon Numarası    : {tel_no}
+
+        """)
+       
+    else:
+        print("Yapmak istediğiniz işlem bulunmamaktadır.")    
+    
+    
+    
+    # kayit_düzenleme = "Kayıt Düzenlemek için 2"
+    # kayit_silmek = "Kayıt Silmek için 3"
+    # kayit_listesi = "Kayıt Listesi için 4"
+    # arama = "Aramayapmak için 5"
+
+    # numaralar = ["1","2","3","4","5"]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+except Exception as mahmud:
+    print(mahmud)
