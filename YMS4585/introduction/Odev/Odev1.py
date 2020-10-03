85bdaefbc59a75177619acc89943f7560e7ec103
cemdalay <71180527+cemdalay@users.noreply.github.com>
2020-10-03 14:10:34 +0300

    Add files via upload

--- YMS4585/introduction/Odev/Odev1.py
+++ YMS4585/introduction/Odev/Odev1.py
@@ -17,6 +17,7 @@
 
 # Yeni bir işlem yapmak istiyor musunuz (Y/N) :
 ###############################################
+
 print("""
 ###############################################
 #                                             # 
@@ -30,57 +31,114 @@ print("""
 
 try:
     
-    telefon_rehberi = ["kayıt ekleme","kayıt düzenleme","kayıt silme","kayıt listesi","arama"]
+    telefon_rehberi =[
+                        {
+
+                            "isim" : "Cem",
+                            "soyadı" : "Dalay",
+                            "tel_no" : "+905312837775"
+
+                        }
+
+
+    ]
+    
+    i = 0
+    islem_numarası = 0
     
     
-    kayit_ekleme = int(input("Kayıt eklemek için 1'e basınız : "))
+    print("""
+    Ana Menü için 0'a basınız
+    Kayıt Eklemek için 1'e basınız
+    Kayıt Düzenlemek için 2'ye basınız
+    Kayıt Silmek için 3'e basınız
+    Kayıt Listesi için 4'e basınız
+    Aramayapmak için 5'e basınız
+    """)
     
-    if kayit_ekleme == 1 :
-        isim = input("Lütfen kaydetmek istediğiniz ismi giriniz :")
-        soyadı = input("Lütfen kaydetmek istediğiniz soyadını giriniz : ")
-        tel_no = input("Lütfen kaydetmek istediğiniz telefon numaranısını giriniz : ")
-
-        print(f"""
-        Adı                 : {isim}
-        Soyadı              : {soyadı}
-        Mail Adresi         : {isim}{soyadı}@cdcable.com
-        Telefon Numarası    : {tel_no}
-
-        """)
-       
-    else:
-        print("Yapmak istediğiniz işlem bulunmamaktadır.")    
+    islem_numarası = [0,1,2,3,4,5]
+    islem_numarası = int(input("Lütfen yapmak istediğiniz işlemin numarasını giriniz : "))
+    isim = []
+    soyadı = []
+    tel_no = []
     
-    
-    
-    # kayit_düzenleme = "Kayıt Düzenlemek için 2"
-    # kayit_silmek = "Kayıt Silmek için 3"
-    # kayit_listesi = "Kayıt Listesi için 4"
-    # arama = "Aramayapmak için 5"
+    while i < islem_numarası :    
+            
+        if islem_numarası == 0 :
+            print("""
+            Ana Menü için 0'a basınız
+            Kayıt Eklemek için 1'e basınız
+            Kayıt Düzenlemek için 2'ye basınız
+            Kayıt Silmek için 3'e basınız
+            Kayıt Listesi için 4'e basınız
+            Arama yapmak için 5'e basınız
+            """)
+         
+        elif islem_numarası == 1 :
+                
+            isim = input("Lütfen kaydetmek istediğiniz ismi giriniz :")
+            soyadı = input("Lütfen kaydetmek istediğiniz soyadını giriniz : ")
+            tel_no = input("Lütfen kaydetmek istediğiniz telefon numaranısını giriniz : ")
 
-    # numaralar = ["1","2","3","4","5"]
+            print(f"""
+            Adı                 : {isim}
+            Soyadı              : {soyadı}
+            Mail Adresi         : {isim}{soyadı}@cdcable.com
+            Telefon Numarası    : {tel_no}
+             """)
+            islem_sonucu = str(input("Kayıt ekleme işlemine devam etmek ister misiniz ?\nDevam etmek için E'ye,Ana Menüye dönmek için O'a basınız."))
+            if islem_sonucu == "0" :
+                print("""
+            Ana Menü için 0'a basınız
+            Kayıt Eklemek için 1'e basınız
+            Kayıt Düzenlemek için 2'ye basınız
+            Kayıt Silmek için 3'e basınız
+            Kayıt Listesi için 4'e basınız
+            Arama yapmak için 5'e basınız
+            """)
+            else:
+                islem_numarası = int(input("Lütfen yapmak istediğiniz işlemin numarasını giriniz : "))
+                break
+                
 
 
+                
+        elif islem_numarası == 2: 
+                
+                # isim = input("Lütfen düzenlemek istediğiniz ismi giriniz :")
+                # soyadı = input("Lütfen kaydetmek istediğiniz soyadını giriniz : ")
+                # tel_no = input("Lütfen kaydetmek istediğiniz telefon numaranısını giriniz : ")
+                
+            isim.append(isim)
+            soyadı.append(soyadı)
+            tel_no.append(tel_no)
+                
+            print(f"""
+            Adı                 : {isim}
+            Soyadı              : {soyadı}
+            Mail Adresi         : {isim}{soyadı}@cdcable.com
+            Telefon Numarası    : {tel_no}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            """)
+        continue  
+            
+            
+               
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    i += 1
 
 except Exception as mahmud:
     print(mahmud)
