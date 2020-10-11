aa15740b723b2323dbf39858919e9a1fdd00eccf
cemdalay <71180527+cemdalay@users.noreply.github.com>
2020-10-11 16:12:53 +0300

    Delete Capitalize_Title_1.py

--- YMS4585/introduction/Lesson10/Capitalize_Title_1.py
+++
@@ -1,8 +0,0 @@
-# .capitalize() => parametrede verilen değerin ilk harfini büyük olarak değiştirir.
-
-
-isim = "cem dalay"
-print(isim.capitalize())  # Cem dalay
-
-# .title() => parametrede verilen değerin içerisindeki her bir kelimenin ilk harfini büyük olarak teslim eder.
-print(isim.title())  # Cem Dalay
