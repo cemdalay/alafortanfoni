6f03e78e8dd756eab0b0c113b355901c0f83538b
cemdalay <71180527+cemdalay@users.noreply.github.com>
2020-10-11 16:12:27 +0300

    Delete Reverse_3.py

--- YMS4585/introduction/Lesson10/Reverse_3.py
+++
@@ -1,8 +0,0 @@
-# .reverse() => dizide var olan elemanları sıralama yapmadan tersine çevirir
-
-sayilar = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-karakterler = ["a", "b", "c", "d", "e", "f"]
-
-sayilar.reverse()
-karakterler.reverse()
-print(sayilar, karakterler)
