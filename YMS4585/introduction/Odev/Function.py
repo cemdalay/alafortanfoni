2c4f9c50f71a5d3d134873091965b46dbb8a9ba0
cemdalay <71180527+cemdalay@users.noreply.github.com>
2020-10-17 11:57:33 +0300

    Add files via upload

--- YMS4585/introduction/Odev/Function.py
+++ YMS4585/introduction/Odev/Function.py
@@ -6,27 +6,20 @@ import os
 os.system('cls')
 
 
-def SayisalDizi():
-    sayisal_dizi = []
+def SayisalDizi(*param):
     dizi = []
-    dizi = input("Lütfen bir dizi giriniz : ")
-    dizi = list(dizi)
-    print(dizi)
-    print(dizi[3:5])
+    for i in param:
 
-    print(type(dizi[0]))
-    for sayi in dizi:
+        if i in param[0:] and (type(i) == float or type(i) == int or type(i) != str):
+            dizi.append(i)
+            dizi.sort()
+            print(f"Girdiğiniz dizide sayı olan değerler :{dizi} ")
 
-        if sayi in dizi[0:] and (type(sayi) == float or type(sayi) == int):
-            sayisal_dizi.append(sayi)
-            print(sayisal_dizi)
-            print(type(sayi))
-        elif sayi in dizi[0:] and (type(sayi) != float or type(sayi) != int):
-            print("Listenizde bulunan sayı olmayan değerler : ", sayi)
-            print(type(sayi))
+        elif i in param[0:] and (type(i) != float or type(i) != int):
+            print("Listenizde bulunan sayı olmayan değerler : ", i)
+
         else:
             print("Tekrar deneyiniz.")
 
 
-SayisalDizi()
-#3, 4, 5, 6, 7, 8, 10.8, "a", "b", "z", "t"
+SayisalDizi(1, 2, 3, 45, 5.6, "a", "b")
